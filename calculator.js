7e407a257bc91e2a7eabf07fdab6b278480bba89
Des Whelan <wheland6@tcd.ie>
2019-03-02 14:02:49 +1300

    reverse pseudocode sample solution

--- calculator.js
+++ calculator.js
@@ -1,1 +1,36 @@
-console.log("Linked to HTML successfully")
+//Create array to store calculator inputs
+var inputs = [];
+var total = 0;
+
+//Create a variable to store current button value
+var temp = "";
+
+//run the script when any button is clicked
+
+    //get the value of the clicked button
+
+    //If it is a number, assign it to temp
+
+    //If it is AC then re-initialise all variables
+
+    //Then add current symbol to the inputs array and clear temp
+    //Translate multiply and divide symobl to mathematical values
+
+    //If it is equals then perform the calculation
+
+        //Use a loop to iterate over each value of the inputs array
+
+            //initialise variables to use in calculations then..
+            //add
+            //subtract
+            //multiply
+            //divide
+    
+        //"Swap the '-' symbol so text input handles it correctly"; Need to figure out what this means!
+
+        //Declare the answer and clear the variables
+
+    //Otherwise add temp and val to inputs and clear temp
+
+
+        
