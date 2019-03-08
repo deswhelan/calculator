3cc76cc06136659df58b8c1eca5093c7277f133f
Des Whelan <wheland6@tcd.ie>
2019-03-09 12:53:13 +1300

    add storeClickedButton function

--- calculator2.js
+++ calculator2.js
@@ -1,3 +1,15 @@
-function calculator () {
-    console.log("js linked to HTML")
+//Create array to store calculator inputs
+var inputs = [];
+var total = 0;
+
+//Create a variable to store current button value
+var temp = "";
+
+
+
+ //Store values of clicked buttons to inputs array
+ function storeClickedButton (buttonValue) {
+    inputs.push(buttonValue);
+    console.log(inputs);
 }
+
