722e8cbff7222603ed0fd2da7a830b8dc6ac2964
Des Whelan <wheland6@tcd.ie>
2019-03-09 12:59:19 +1300

    add allClear function

--- calculator2.js
+++ calculator2.js
@@ -5,11 +5,16 @@ var total = 0;
 //Create a variable to store current button value
 var temp = "";
 
-
-
- //Store values of clicked buttons to inputs array
- function storeClickedButton (buttonValue) {
+ //Store values of clicked number or operator buttons to inputs array
+ function storeClickedButton(buttonValue) {
     inputs.push(buttonValue);
     console.log(inputs);
 }
 
+//Re-initialise all variables when AC is clicked
+function allClear() {
+    inputs = [];
+    total = 0;
+    temp = "";
+    console.log(inputs, total, temp);
+};
