d6c5a46d89ceedf3fa412833670d2cdd5c239a7d
Des Whelan <wheland6@tcd.ie>
2019-03-09 14:57:05 +1300

    add displayClickedButton function

--- calculator2.js
+++ calculator2.js
@@ -1,25 +1,70 @@
 //Create array to store calculator inputs
-var inputs = [];
+var clickedButtons = [];
 var total = 0;
 
 //Create a variable to store current button value
-var temp = "";
+var temporaryValue = [];
 
- //Store values of clicked number or operator buttons to inputs array
- function storeClickedButton(buttonValue) {
-    inputs.push(buttonValue);
-    display(inputs);
+//Store values of clicked buttons to clickedButtons array
+function storeClickedButton(buttonValue) {
+    displayClickedButton(buttonValue);
+    clickedButtons.push(buttonValue);
+}
+
+
+function displayClickedButton(buttonValue) {
+    if(!isNaN(buttonValue) || buttonValue == "."){
+        temporaryValue.push(buttonValue);
+        display(temporaryValue.join(""))
+    } else {
+        display(buttonValue);
+        temporaryValue = [];
+    }
 }
 
 //Re-initialise all variables when AC is clicked
 function allClear() {
-    inputs = [];
+    clickedButtons = [];
     total = 0;
-    temp = "";
+    temporaryValue = [];
     display(0);
 }
 
-//show answer or current number on calculator screen
+//convert individual inputs to appropriate display values
+// function convertToDisplay() {
+//     var displayValue = clickedButtons.reduce((a,c) => a + c);
+//     var sliceCounter = 0;
+
+//     clickedButtons.forEach(function (element) {
+//         if(!isNaN(element)){
+//             sliceCounter++
+//         } else {
+//             console.log(sliceCounter);
+//             return;
+//         }
+//     });
+
+//     display(temporaryValue);
+// }
+
+function defineDisplayValue() {
+    var sliceCounter = 0;
+
+    clickedButtons.forEach(function (element) {
+        if(!isNaN(element)){
+            sliceCounter++
+        } else {
+            console.log(sliceCounter);
+            return;
+        }
+    });
+
+    var displayValue = "";
+
+    return displayValue;
+}
+
+//show current number, symbol or answer on calculator screen
 function display(displayValue) {
     document.getElementById("display").innerHTML = displayValue;
 }
